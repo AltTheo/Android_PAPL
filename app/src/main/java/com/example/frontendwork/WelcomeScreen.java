62cd8379041938b038b58958099e0350763afd68
UP968909 <up968909@myport.ac.uk>
2022-01-27 06:21:03 +0000

    Error message Implemented

--- app/src/main/java/com/example/frontendwork/WelcomeScreen.java
+++ app/src/main/java/com/example/frontendwork/WelcomeScreen.java
@@ -1,14 +1,18 @@
 package com.example.frontendwork;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageSwitcher;
 import android.widget.TextView;
 
 public class WelcomeScreen extends AppCompatActivity {
@@ -18,23 +22,31 @@ public class WelcomeScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
         setTitle("Home");
-        MediaPlayer media = MediaPlayer.create(this, R.raw.sound);
-        media.start();
+//        MediaPlayer media = MediaPlayer.create(this, R.raw.sound);
+//        media.start();
     }
 
     @SuppressLint("SetTextI18n")
     public void HomePage(View v){
         Intent i = new Intent(this, MainActivity.class);
-//        EditText tv = findViewById(R.id.nameText);
-//        String name = tv.getText().toString();
-//        ((TextView) findViewById(R.id.wlcDisplay)).setText("HELLO THERE, "+name);
-        startActivity(i);
+        EditText tv = findViewById(R.id.nameText);
+        String name = tv.getText().toString();
+        if(tv.getText().toString().equals("")){
+            tv.setError("Please enter your name");
+        }
+        else{
+            startActivity(i);
+        }
+
 //
 //        Button b = findViewById(R.id.nextPage);
 //        MediaPlayer media = MediaPlayer.create(this, R.raw.sound);
 //        media.start();
     }
 
-
+    public void soundClick(View v) {
+        MediaPlayer media = MediaPlayer.create(this, R.raw.sound);
+        media.start();
+    }
 
 }
